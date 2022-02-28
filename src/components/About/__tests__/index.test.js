b97a80783575e9e7ed943ec4012d6688911e6320
jbryanmcgrath <90098243+jbryanmcgrath@users.noreply.github.com>
2022-02-28 16:15:48 -0500

    20.3

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,20 +1,19 @@
-import React from "react";
-import { render, cleanup } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
-import About from "..";
+// __tests__/About.test.js
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
 
-
-afterEach(cleanup)
+afterEach(cleanup);
 
 describe('About component', () => {
-    //First Test
-    it('renders', () => {
-        render(<About />)
-    })
-
-    it('matches snapshot DOM node structure', () => {
-        const { asFragment } = render(<About />);
-        expect(asFragment()).toMatchSnapshot();
-    })
-    //Second Test
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
